8c2addd477af32918d4c5baccd9775fde700b297
siddhant <siddhant.chawla9@gmail.com>
2019-07-12 01:12:54 +0530

    Files added

--- election/models.py
+++ election/models.py
@@ -1,11 +1,5 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class AuthGroup(models.Model):
@@ -141,7 +135,7 @@ class Election(models.Model):
 
 
 class Faculty(models.Model):
-    faculty = models.OneToOneField('User',models.CASCADE)
+    faculty = models.OneToOneField(AuthUser,models.CASCADE)
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     phone = models.IntegerField()
@@ -153,7 +147,7 @@ class Faculty(models.Model):
 
 
 class Result(models.Model):
-    user = models.ForeignKey('User', models.SET_NULL,null=True)
+    user = models.ForeignKey(AuthUser, models.SET_NULL,null=True)
     session_id = models.IntegerField(primary_key=True)
 
     class Meta:
@@ -162,7 +156,7 @@ class Result(models.Model):
 
 
 class Student(models.Model):
-    student = models.OneToOneField('User',models.CASCADE)
+    student = models.OneToOneField(AuthUser,models.CASCADE)
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     phone = models.IntegerField()
@@ -173,15 +167,15 @@ class Student(models.Model):
         db_table = 'student'
 
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=100)
-    isAdmin = models.IntegerField(db_column='isAdmin')  # Field name made lowercase.
+# class User(models.Model):
+#     user_id = models.AutoField(primary_key=True)
+#     username = models.CharField(max_length=20)
+#     password = models.CharField(max_length=100)
+#     isAdmin = models.IntegerField(db_column='isAdmin')  # Field name made lowercase.
 
-    class Meta:
-        managed = False
-        db_table = 'user'
+#     class Meta:
+#         managed = False
+#         db_table = 'user'
 
 
 class Vote(models.Model):
